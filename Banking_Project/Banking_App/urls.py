9384b2bfd4d0802cb8f5d68b9d5ff70507364aa0
DileepKumarMantena <deelipkumar261997@gmail.com>
2024-08-28 18:19:09 +0530

    added code

--- Banking_Project/Banking_App/urls.py
+++ Banking_Project/Banking_App/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('  /', views.dashboard, name='dashboard'),
     path('logout/', views.user_logout, name='logout'),
     path('add-account/', views.add_account, name='add_account'),
     path('view_account/<int:account_id>/', views.view_account, name='view_account'),
